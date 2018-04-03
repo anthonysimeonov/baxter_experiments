bbc41db24c5c5b0c288d16cf21279179d09fa8da
zephirefaith <priyam8parashar@gmail.com>
2018-04-03 16:18:36 -0700

    pre-skeleton stage :P

---
+++ scripts/reinforcement_learner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Author: Priyam Parashar
+# Class: ReinforcementLearner()
+# Description:  Generic interface which can accept different learner functions
+#               adhering to required output-input format. Includes all other
+#               helper functions for managing episodic learning
