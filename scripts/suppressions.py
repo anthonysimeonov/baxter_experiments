31b14ef3b28c137bf2d03a682b0822508142c7d8
anthonysimeonov <anthsimeonov@gmail.com>
2018-04-26 11:15:40 -0700

    added gravity, working nice but no user arguments in main

--- scripts/suppressions.py
+++ scripts/suppressions.py
@@ -1,11 +1,11 @@
-
+#!/usr/bin/env python
 
 import rospy
 import std_msgs.msg as std_msgs
 import time
 
 class baxter_suppressions():
-    def __init__(self, left=True, right=True, contact=True, collisions=True, body=True, overwrench=True):
+    def __init__(self, left=True, right=True, contact=True, collisions=True, body=True, overwrench=True, gravity=True):
         self.left_contact_pub = rospy.Publisher('/robot/limb/left/suppress_contact_safety', std_msgs.Empty, queue_size=1)
         self.right_contact_pub = rospy.Publisher('/robot/limb/right/suppress_contact_safety', std_msgs.Empty, queue_size=1)
 
@@ -18,6 +18,10 @@ class baxter_suppressions():
         self.left_overwrench_pub = rospy.Publisher('/robot/limb/left/suppress_hand_overwrench_safety', std_msgs.Empty, queue_size=1)
         self.right_overwrench_pub = rospy.Publisher('/robot/limb/right/suppress_hand_overwrench_safety', std_msgs.Empty, queue_size=1)
 
+        self.left_gravity_pub = rospy.Publisher('/robot/limb/left/suppress_gravity_compensation', std_msgs.Empty, queue_size=1)
+        self.right_gravity_pub = rospy.Publisher('/robot/limb/right/suppress_gravity_compensation', std_msgs.Empty, queue_size=1)
+
+
         self._left = left
         self._right = right
 
@@ -27,40 +31,51 @@ class baxter_suppressions():
         self._collisions = collisions
         self._body = body
         self._overwrench = overwrench
+        self._gravity = gravity
 
-        self.suppressions = [self._contact, self._collisions, self._body, self._overwrench]
+        self.suppressions = [self._contact, self._collisions, self._body, self._overwrench, self._gravity]
 
     def suppress_right(self):
+        msg = std_msgs.Empty()
         if self._right:
             if self._contact:
-                self.right_contact_pub.publish(std_msgs.Empty)
+                self.right_contact_pub.publish(msg)
             if self._collisions:
-                self.right_collisions_pub.publish(std_msgs.Empty)
+                self.right_collisions_pub.publish(msg)
             if self._body:
-                self.right_body_pub.publish(std_msgs.Empty)
+                self.right_body_pub.publish(msg)
             if self._overwrench:
-                self.right_overwrench_pub.publish(std_msgs.Empty)
+                self.right_overwrench_pub.publish(msg)
+            if self._gravity:
+                self.right_gravity_pub.publish(msg)
 
     def suppress_left(self):
+        msg = std_msgs.Empty()
         if self._left:
             if self._contact:
-                self.left_contact_pub.publish(std_msgs.Empty)
+                self.left_contact_pub.publish(msg)
             if self._collisions:
-                self.left_collisions_pub.publish(std_msgs.Empty)
+                self.left_collisions_pub.publish(msg)
             if self._body:
-                self.left_body_pub.publish(std_msgs.Empty)
+                self.left_body_pub.publish(msg)
             if self._overwrench:
-                self.left_overwrench_pub.publish(std_msgs.Empty)
+                self.left_overwrench_pub.publish(msg)
+            if self._gravity:
+                self.left_gravity_pub.publish(msg)                
 
 
 def main():
     rospy.init_node('baxter_suppressor', anonymous = True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(15)
     suppressions = baxter_suppressions()
-    suppressions.suppress_right()
-    suppressions.suppress_left()
+
+    print("Turning on all relevant suppressions for the baxter... stay safe... \n")
+
     while not rospy.is_shutdown():
+        suppressions.suppress_right()
+        suppressions.suppress_left()
         rate.sleep()
+
 
 if __name__ == '__main__':
     main()
